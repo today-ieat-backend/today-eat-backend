0464c08557c99096128c3a8f36b0bdfcb0d7f8a3
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-07-10 11:04:40 +0900

    Add router for Send recommended food

--- routes/menu.js
+++ routes/menu.js
@@ -1,6 +1,55 @@
 const express = require('express');
+const { Op } = require('sequelize');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const Menu = require('../models/menu');
 
 const router = express.Router();
+
+
+try {
+    fs.readdirSync('uploads');
+} catch (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+};
+
+const upload = multer({
+    storate: multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, 'uploads');
+        },
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+
+//메뉴 카테고리 지정하면, 추천 음식 보내기
+router.get('/', async (req, res, next) => {
+    const { category1, category2, category3 } = req.query;
+
+    try {
+        const menuList = await Menu.findAll({
+            where: {
+                [Op.or]: [{ category1 }, { category2 }, { category3 }],
+            },
+            order: [['like', 'DESC'], ['createdAt', 'DESC']],
+        })
+        res.json({ "ok": true, "menu-list": menuList });
+    } catch (error) {
+        console.error(error);
+        res.json({ "ok": false, "message": '실패' })
+    }
+
+});
+
+
 
 
 module.exports = router;
